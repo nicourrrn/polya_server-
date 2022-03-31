74ddd3aa313caed1a4f0517964fb821f7c036446
Vadym Sorokin <nicourrrn@gmail.com>
2022-03-31 16:45:48 +0300

    add update path

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 import os
 
 app = FastAPI()
 
-def make_path(file_name: str):
+
+async def make_path(file: UploadFile) -> int:
     try:
-        os.mkdir(f"paths/{file_name}")
-    except: pass
-    file = open(f"files/{file_name}", 'rb')
+        os.mkdir(f"paths/{file.filename}")
+    except:
+        pass
     i = 0
     while True:
-        tmp = file.read(1048576)
+        tmp = await file.read(1048576)
         if len(tmp) == 0:
             break
-        with open(f"paths/{file_name}/path_{i}", 'wb') as path_file:
+        with open(f"paths/{file.filename}/path_{i}", 'wb') as path_file:
             path_file.write(tmp)
         i += 1
-        print(i * 1024)
+    return i
 
-#make_path("app.apk")
 
 def make_one(file_name: str):
     file = open(f"new_{file_name}", 'wb')
@@ -28,10 +28,17 @@ def make_one(file_name: str):
             tmp = path.read()
             file.write(tmp)
 
+
+@app.post("/upload")
+async def upload_file(file: UploadFile):
+    return {"count": await make_path(file)}
+
+
 @app.get("/{file_name}")
 async def get_data(file_name: str):
     return {"count": len(os.listdir(f"paths/{file_name}"))}
 
+
 @app.get("/{file_name}/{path}", response_class=FileResponse)
-async def upload_file(file_name: str, path: int):
+async def download_file(file_name: str, path: int):
     return FileResponse(f"paths/{file_name}/path_{path}")
